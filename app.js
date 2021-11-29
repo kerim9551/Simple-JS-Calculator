154c84124351acf65051c444aba9d2a81fb072eb
kerim9551 <kerim9551@gmail.com>
2021-11-29 12:27:09 +0500

    added the required variables

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var input = document.getElementById('input');
+var number = document.querySelectorAll('.numbers div');
+var operator = document.querySelectorAll('.operators div');
+var result = document.getElementById('result');
+var clear = document.getElementById('clear');
+var resultDisplayed = false;
