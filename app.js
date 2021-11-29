f136a6cb504402d4be3a655ab0c4e5644727a1da
kerim9551 <kerim9551@gmail.com>
2021-11-29 12:39:34 +0500

    added click handlers to number buttons

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ var operator = document.querySelectorAll('.operators div');
 var result = document.getElementById('result');
 var clear = document.getElementById('clear');
 var resultDisplayed = false;
+
+
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function(e) {
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
+
+        if (resultDisplayed === false) {
+            input.innerHTML += e.target.innerHTML;
+        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            resultDisplayed = false;
+            input.innerHTML += e.target.innerHTML;
+        } else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
